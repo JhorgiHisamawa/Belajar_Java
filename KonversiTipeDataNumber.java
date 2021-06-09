bb45415dff2f05659fa9bb94eba40d8fc203c518
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-06-09 20:25:58 +0700

    Konversi Tipe Data Number

--- KonversiTipeDataNumber.java
+++ KonversiTipeDataNumber.java
@@ -20,6 +20,15 @@ public class KonversiTipeDataNumber {
     int NamaVariabelInt2 = (int) NamaVariabelLong2 ;
     short NamaVariabelShort2 = (short) NamaVariabelInt2 ;
     byte NamaVariabelByte2 = (byte) NamaVariabelShort2 ;
+    
+    
+/* HATI-HATI 
+jika konversi paksa melebihi batas nilai dari tipe data number
+*/
+    int NamaInteger = 1000 ;
+    byte NamaByte = (byte) NamaInteger // ini akan mengakibatkan number overflow
+      
+// jadi setelah byte mencapai angka 127, byte akan kembali ke -128, terus akan ditambahkan sampai ia akan mencapai 1000
         }
 }
     
