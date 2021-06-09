5a581903addcb74f60e69e4aec2adaea6dacfb00
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-06-09 21:27:39 +0700

    Tipe Data String

---
+++ TipeDataString.java
@@ -0,0 +1,16 @@
+/* 
+1. default value String adalah null 
+2. untuk penulisan string harus S besar seperti "String" kalo s kecil seperti "string" akan error
+*/
+
+public class TipeDataString {
+  public static void main(String[] args) {
+    String Nama = "Ini" ;
+    String Variabel = "Nama Variabel" ; 
+    String NamaVariabel = Nama + " " + Variabel ; //untuk menggabungkan string
+    
+    System.out.println(Nama)  // Ini
+    Sistem.out.println(Variabel) //Nama Variabel
+    System.out.println(NamaVariabel) ; // Ini Nama Variabel
+          }
+}
