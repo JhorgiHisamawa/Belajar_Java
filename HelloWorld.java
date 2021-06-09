383d0b2cf64f6e386237ee9acd8b712554cb0fae
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-06-09 15:14:44 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld {                                                               //nama file harus sama dengan HelloWorld.java
+  public static void main(String[] args) {                                              //wajib menggunakan main function
+    // Code Program
+    
+    
+    System.out.println("Hello World");                                                  //harus ada titik coma 
+    // Code Program
+      }
+};
+      
+/*
+
+PETUNJUK
+1. nama file yang ada di public class akan otomatis dibuatkan oleh IntelijIDEA
+2. nama kelas di java tidak boleh mengandung spasi (pake Camel case)
+3. kalo nama class biasanya ditulis dengan huruf besar
+4. di src -> (klik kanan) java class -> ketik nama di new java class -> pilih option Class -> lalu enter
+5. java itu case sensitive
+
+Cara manual running program
+1. file ini harus di compile dulu
+2. cara compile = buka cmd / terminal -> harus di directory src dulu -> ketikan javac(spasi)nama file(dot)java atau javac HelloWorld.java
+3. tunggu sampai file berubah menjadi (dot)class atau HelloWorld.class
+4. untuk menjalankan kode program ketikkan java(spasi)(nama file) atau java HelloWorld
+
+Cara cepat
+1. klik filenya yang "HelloWorld.java" terus klik kanan "Run HelloWorld.main"
+
+*/
