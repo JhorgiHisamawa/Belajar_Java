dc7bb1ce1b0f89ced7f673b8c6c8feb36a246013
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-06-09 20:43:32 +0700

    Tipe Data Char

---
+++ TipeDataChar.java
@@ -0,0 +1,33 @@
+/*
+1. Data Character di java dipresentasaikan oleh tipe data char
+2. Untuk membuat data char di java, kita bisa menggunakan tanda ' (petik satu) di awal dan di akhir karakter
+*/
+
+public class TipeDataChar {
+  public static void main(String[] args) {
+    char j = 'J' ;
+    char k = 'K' ;
+    char l = 'L' ;
+    
+    System.out.println(j) ; 
+    System.out.println(k) ;
+    System.out.println(l) ;
+    
+    /*  ---Terminal---
+    J
+    K
+    L
+    */
+    
+    System.out.print(j) ;
+    System.out.print(k) ;
+    System.out.print(l) ;
+    
+    /* ---Terminal---
+    JKL
+    
+    nb: jd println itu adalah setelah baris code (enter), jadi kalau print biasa akan jadi satu baris code
+    */
+    
+      }
+}
