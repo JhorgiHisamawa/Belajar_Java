dbed3b3585e2d7e2524b1742524cee57fc342384
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-06-09 21:02:37 +0700

    Tipe Data Boolean

---
+++ TipeDataBoolean.java
@@ -0,0 +1,14 @@
+/*
+tipe data boolean mempunyai 2 nilai yaitu : true dan false
+default value adalah false
+*/
+
+public class TipeDataBoolean {
+  public class void main (String[] args) {
+    boolean benar = true ;
+    boolean salah = false ;
+    
+    System.out.println(benar) //true
+    System.out.println(salah) //false
+    }
+}
