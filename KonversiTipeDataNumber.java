7504301fd0eb212773e6eb3b539aa6e57f91d259
JhorgiHisamawa <80043758+JhorgiHisamawa@users.noreply.github.com>
2021-06-09 20:13:02 +0700

    Konversi Data Number

---
+++ KonversiTipeDataNumber.java
@@ -0,0 +1,25 @@
+/*
+Widening Casting = konversi otomatis dari java-nya yaitu:
+byte-short-int-long-float-double
+Narrowing Casting = konversi manual yaitu:
+double-float-long-int-short-byte
+*/
+
+class KonversiTipeDataNumber {
+  public static void main(String[] args) {
+    
+    byte NamaVariabelByte = 10 ;
+    short NamaVariabelShort = NamaVariabelByte ;
+    int NamaVariabelInt = NamaVariabelShort ;
+    long NamaVariabelLong = NamaVariabelInt ;
+    float NamaVariabelFloat = NamaVariabelLong ;
+    double NamaVariabelDouble = NamaVariabelFLoat ;
+    
+    float NamaVariabelFloat2 = (float) NamaVariabelDouble ;
+    long NamaVariabelLong2 = (long) NamaVariabelFLoat2 ;
+    int NamaVariabelInt2 = (int) NamaVariabelLong2 ;
+    short NamaVariabelShort2 = (short) NamaVariabelInt2 ;
+    byte NamaVariabelByte2 = (byte) NamaVariabelShort2 ;
+        }
+}
+    
